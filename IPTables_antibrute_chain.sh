2a5a4d6a3ec552ddcfa83c28133828f4a0bdb8a3
Giuseppe De Marco <giuseppe.demarco@unical.it>
2019-11-02 17:05:10 +0100

    Update IPTables_antibrute_chain.sh

--- IPTables_antibrute_chain.sh
+++ IPTables_antibrute_chain.sh
@@ -3,29 +3,21 @@ set -e
 set -x
 
 IPT="/sbin/iptables"
-IPTABLES=$IPT
-CHAIN=BOGUS_NET
+CHAIN=ANIBRUTEFORCE_TCP
+HITCOUNT=3 # 2 syn connection (<3)
+SECONDS=20 # in 20 seconds are allowed
+SSH_PORT=22
 
 #######
 # creo la chain
 #######
 $IPT -N $CHAIN
 
-###############################################
-# Mi difendo dallo spoofing
-###############################################
-$IPT -A $CHAIN -s 10.0.0.0/8 -j DROP 
-$IPT -A $CHAIN -s 169.254.0.0/16 -j DROP
-$IPT -A $CHAIN -s 172.16.0.0/12 -j DROP
-$IPT -A $CHAIN -s 127.0.0.0/8 -j DROP
-$IPT -A $CHAIN -s 192.168.0.0/24 -j DROP
-$IPT -A $CHAIN -s 192.168.1.0/24 -j DROP
-$IPT -A $CHAIN -s 192.168.10.0/24 -j DROP
-$IPT -A $CHAIN -s 224.0.0.0/4 -j DROP
-$IPT -A $CHAIN -d 224.0.0.0/4 -j DROP
-$IPT -A $CHAIN -s 240.0.0.0/5 -j DROP
-$IPT -A $CHAIN -d 240.0.0.0/5 -j DROP
-$IPT -A $CHAIN -s 0.0.0.0/8 -j DROP
-$IPT -A $CHAIN -d 0.0.0.0/8 -j DROP
-$IPT -A $CHAIN -d 239.255.255.0/24 -j DROP
-$IPT -A $CHAIN -d 255.255.255.255 -j DROP
+# --rcheck: Check if the source address of the packet is  currently  in  the list.
+# --update: Like  --rcheck,  except it will update the "last seen" timestamp if it matches.
+
+$IPT -A $CHAIN -p tcp -m tcp --dport $SSH_PORT -m state --state NEW -m recent --set --name sshguys --rsource
+$IPT -A $CHAIN -p tcp -m tcp --dport $SSH_PORT -m state  --state NEW  -m recent --rcheck --seconds $SECONDS --hitcount $HITCOUNT --rttl --name sshguys --rsource -j LOG --log-prefix "BLOCKED SSH (brute force)" --log-level 4 -m limit --limit 1/minute --limit-burst 5
+$IPT -A $CHAIN -p tcp -m tcp --dport $SSH_PORT -m recent --rcheck --seconds $SECONDS --hitcount $HITCOUNT --rttl --name sshguys --rsource -j REJECT --reject-with tcp-reset
+$IPT -A $CHAIN -p tcp -m tcp --dport $SSH_PORT -m recent --update --seconds $SECONDS --hitcount $HITCOUNT --rttl --name sshguys --rsource -j REJECT --reject-with tcp-reset
+$IPT -A $CHAIN -p tcp -m tcp --dport $SSH_PORT -m state --state NEW,ESTABLISHED  -j ACCEPT
